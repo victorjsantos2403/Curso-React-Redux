2a156e730f5d73e999695a9eac53cb69ceeab71a
victorjsantos2403 <victorjsantos240301@gmail.com>
2022-08-20 23:07:29 -0300

    primeiro contato com o jsx e react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import ReactDOM from'react-dom';
+import React  from 'react';
 
 const el = document.getElementById('root');
-ReactDOM.render('Olá mundo react-native', el);
+// Exemplo com o reactDom
+// ReactDOM.render('Olá mundo react-native', el);
+
+// Exemplo com o jsx e React
+
+ReactDOM.render(
+    <div>Olá mundo react-native</div>,
+    el
+);
