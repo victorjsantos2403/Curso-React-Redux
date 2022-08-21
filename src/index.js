c0e3ae3fc9cb671adb4b5f4e820fcaea20946db8
victorjsantos2403 <victorjsantos240301@gmail.com>
2022-08-20 23:16:21 -0300

    Primeiro uso do css com o react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from'react-dom';
 import React  from 'react';
 
+import './index.css';
+
 const el = document.getElementById('root');
 // Exemplo com o reactDom
 // ReactDOM.render('Olá mundo react-native', el);
