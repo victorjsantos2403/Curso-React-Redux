cbc8c9624e1e97e9f255d09fc790e665df71e6b8
victorjsantos2403 <victorjsantos240301@gmail.com>
2022-08-20 23:26:06 -0300

    Uso do jsx no component

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,4 +1,8 @@
 import React from "react";
 export default function Primeiro() {
-    return <h2>Primeiro Componente</h2>;
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+        </div>
+    );
 }
